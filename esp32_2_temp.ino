4e8df95637322eb1429bbf556d2d55821bba2f20
mhampton2003 <74636177+mhampton2003@users.noreply.github.com>
2025-01-27 15:59:21 -0700

    Update esp32_2_temp.ino

--- esp32_2_temp.ino
+++ esp32_2_temp.ino
@@ -1,38 +1,43 @@
+/**
+=================================================================
+ RV Intelligence Device
+ Emmanuel Loria, Jordan Krause, Maya Hampton
+ Date 11/20/2024
+ Script to allow ESP32 to connect via BlueTooth and read temperature
+ https://youtu.be/bsss1MyXVNI?si=Iq3n1tNmUvDSUoeq
+    > used to learn how to set up BlueTooth connection
+=================================================================
+**/
+
 #include <DHT.h>
 #include "BluetoothSerial.h"
 #include <ArduinoJson.h>
 
 BluetoothSerial SerialBT; // Initialize Bluetooth
 
-DHT dht(4, DHT11);
+DHT dht(4, DHT11); // iniitalize temperature sensor
 
 void setup() {
-  // put your setup code here, to run once:
 
   Serial.begin(115200);   // Serial monitor
   SerialBT.begin("ESP32_1"); // Bluetooth device name
   Serial.println("Bluetooth started, waiting for connection...");
-
   dht.begin();
   delay(2000);
-
   Serial.begin(115200);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+// if the ESP32 is connected then run
   if (SerialBT.connected()) {
-    
+    // print data through the BlueTooth Serial port
     float temp = dht.readTemperature();
     float humidity = dht.readHumidity();
-    SerialBT.print("Temp 1: ");
     SerialBT.print((temp * 1.8) + 31);
-    SerialBT.print(" F \n");
+    SerialBT.print(" F ");
     delay(2000);
 
   }
-
-
 }
